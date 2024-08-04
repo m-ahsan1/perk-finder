659e9a078dc61433c9e77b6b438aa05bc4f5e16a
Muhammad Ahsan <ahsank0811@gmail.com>
2024-08-05 04:28:27 +0500

    add tailwind, nav, search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import MainPage from "./MainPage";
+import Nav from "./components/Nav";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <MainPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+function Nav() {
+  return (
+    <nav className="bg-slate-900 text-white w-full h-[80px] flex items-center">
+      <ul className="w-full flex flex-row items-center justify-between">
+        <li className="m-2 p-2 font-mono font-semibold text-lg">
+          <a href="/">PerkFinder</a>
+        </li>
+
+        <button className="bg-green-500 rounded-full m-2 p-3 px-6 hover:scale-105 duration-300 font-semibold">
+          <p>Add Offer</p>
+        </button>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
