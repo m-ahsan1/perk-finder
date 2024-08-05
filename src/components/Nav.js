d45196e101c6ce1d0e0a7cf063e2f9a1cd0103f9
Muhammad Ahsan <ahsank0811@gmail.com>
2024-08-05 05:33:42 +0500

    improve ui

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ function Nav() {
   return (
     <nav className="bg-slate-900 text-white w-full h-[80px] flex items-center">
       <ul className="w-full flex flex-row items-center justify-between">
-        <li className="m-2 p-2 font-mono font-semibold text-lg">
+        <li className="m-2 p-2 font-mono font-semibold text-2xl hover:scale-105 duration-300">
           <a href="/">PerkFinder</a>
         </li>
 
--- src/components/PerkCard.js
+++ src/components/PerkCard.js
@@ -1,21 +1,53 @@
 import React from "react";
+import ReactCardFlip from "react-card-flip";
 
 function PerkCard() {
+  const [isFlipped, setIsFlipped] = React.useState(false);
+
+  function flipCard() {
+    setIsFlipped(!isFlipped);
+  }
+
   return (
-    <div className="bg-gray-800 rounded-2xl text-gray-100 max-w-md h-[200px] m-4 p-4 flex items-center justify-center">
-      <div>
-        <h1 className="text-2xl font-bold text-center pb-6">50% OFF</h1>
-        <div className="grid grid-cols-2">
+    <div>
+      <ReactCardFlip flipDirection="vertical" isFlipped={isFlipped}>
+        <div
+          className="bg-gray-800 rounded-2xl text-gray-100 max-w-md h-[200px] m-4 p-4 flex items-center justify-center"
+          onClick={flipCard}
+        >
           <div>
-            <p>Domino's</p>
-            <p>Food</p>
-          </div>
-          <div>
-            <p>Meezan Bank</p>
-            <p>Visa Platinum Debit Card</p>
+            <h1 className="text-2xl font-bold text-center pb-6">50% OFF</h1>
+            <div className="grid grid-cols-2">
+              <div className="">
+                <p className="my-2 w-fit bg-green-500 font-semibold rounded-full p-2 text-center">
+                  Domino's
+                </p>
+                <p className="w-fit bg-green-500 font-semibold rounded-full p-2">
+                  Food
+                </p>
+              </div>
+              <div className="">
+                <p className="my-2 w-fit bg-green-500 font-semibold rounded-full p-2">
+                  Meezan Bank
+                </p>
+                <p className="w-fit bg-green-500 font-semibold rounded-full p-2">
+                  Visa Platinum Debit Card
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+
+        <div
+          className="bg-gray-800 rounded-2xl text-gray-100 max-w-md h-[200px] m-4 p-4 flex items-center justify-center"
+          onClick={flipCard}
+        >
+          <div>
+            <h1 className="font-semibold text-lg py-2">Terms & Conditions</h1>
+            <p>1. Offer valid for dine-in, takeaway, and delivery.</p>
+          </div>
+        </div>
+      </ReactCardFlip>
     </div>
   );
 }
